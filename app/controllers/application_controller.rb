c095a177042c6d2e60765404c32a7cabe641451d
nahwinrajan <nahwin.rajan@gmail.com>
2016-10-19 12:31:21 +0700

    finish add users and  authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def not_found
     respond_to do |format|
@@ -10,6 +12,12 @@ class ApplicationController < ActionController::Base
   rescue ActionController::UnknownFormat
     render status: 404, text: "nope"
   end
+
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
+      devise_parameter_sanitizer.permit(:account_update, keys:[:name])
+    end
 
   private
     def current_cart
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,21 @@
 Rails.application.routes.draw do
-  root 'products#index'
+  devise_for :users
+
+  devise_scope :user do
+    authenticated :user do
+      root 'products#index', as: 'products'
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: 'signin'
+    end
+  end
 
   resources :products
   resources :clients
   resources :line_items
   resources :carts
-  
+
 
   get '*unmatched_route', to: 'application#not_found'
 end
